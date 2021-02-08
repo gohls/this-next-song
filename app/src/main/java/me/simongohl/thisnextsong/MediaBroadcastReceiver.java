e3c620490a91c42f3a7b023167696426f2e599f8
gohls <sagohl@gmail.com>
2021-02-07 21:35:38 -0500

    WIP: listen for system media changes

--- app/src/main/java/me/simongohl/thisnextsong/MediaBroadcastReceiver.java
+++ app/src/main/java/me/simongohl/thisnextsong/MediaBroadcastReceiver.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class MediaBroadcastReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
